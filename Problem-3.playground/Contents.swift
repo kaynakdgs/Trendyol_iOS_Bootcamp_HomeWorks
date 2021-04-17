83a9af339eaa48bb84b8999294c878cd6b1c7e8b
Doğuş Kaynak <doguskaynak@gmail.com>
2021-04-17 19:23:56 +0300

    ProjectEulerSolutions

--- Problem-3.playground/Contents.swift
+++ Problem-3.playground/Contents.swift
@@ -1,42 +1,4 @@
-
-//import Foundation
-//
-//class factor {
-//    var arrayFactors: [Int]
-//
-//    init () {
-//        self.arrayFactors = []
-//    }
-//
-//    func findFactors(number:Int) -> Int {
-//        var i = 2
-//        var nextNumber: Int = number
-//
-//        while nextNumber > 1 {
-//            if nextNumber % i == 0 {
-//                arrayFactors.append(i)
-//                nextNumber = Int(nextNumber / i)
-//                while nextNumber % i == 0 {
-//                    nextNumber = Int(nextNumber / i)
-//                }
-//
-//                print("factor \(i) leaves number \(nextNumber)")
-//            }
-//
-//            i += 1
-//        }
-//
-//        return arrayFactors.removeLast()
-//    }
-//}
-//
-//let myFactor = factor()
-//let number = 600851475143
-//print("largest prime factor of \(number) is \(myFactor.findFactors(number: number))")
-
-
 import UIKit
-
 
 func largestPrimeFactor (givenNum : Int) -> Int {
 
