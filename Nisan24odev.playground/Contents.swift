cde660ef208629999b7be51ffbfb822686307a1e
Doğuş Kaynak <doguskaynak@gmail.com>
2021-04-24 20:09:24 +0300

    24nisanHomeWork

---
+++ Nisan24odev.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+//MARK: ODEV: Girilen sayının asal olup olmadığını bulan bir extension yazınız.
+
+extension Int {
+    func isPrime() -> Bool {
+        guard self > 1 else {
+            return false
+        }
+        
+        for num in 2..<self {
+            if self % num == 0 {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+47.isPrime()
+
+
+//MARK: ODEV: İki Parametreli ve Farklı tipli bir generic örneği yapınız.
+
+func multipleValues<T: Comparable, S: Comparable>(_ first: T, _ second: S) -> (T,S) {
+    return (first,second)
+}
+
+let result = multipleValues("a", 3)
+print(result)
